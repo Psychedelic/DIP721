efb0a2f34576dbafbe3c7b1cc405bae6452061fe
Helder Oliveira <heldrida@gmail.com>
2022-02-16 22:29:16 +0000

    refactor: 💡 use split scripts in deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,87 +1,45 @@
 #!/bin/bash
 
+source "./.scripts/dfx-identity.sh"
+
 if [[ -z $1 ]];
 then
     printf "💎 DIP-721 Deploy Script:\n\n   usage: npm run dip721:deploy <local|ic|other> [reinstall]\n\n"
+
     exit 1;
 fi
 
+# Args
 NETWORK=$1
-NFT="nft" # canister name
+MODE=$2
+
+# Defaults
 TOKENSTRING="TKN"
-TOKENNAME="Non Fungable Token"
-PRINCIPALID=$(dfx identity get-principal)
-YARNORNPM="npm run" #`npm run` or `yarn`
+TOKENNAME="Non Fungible Token"
+
+OWNER_PRINCIPAL_ID="$DEFAULT_PRINCIPAL_ID"
+CAP_HISTORY_ROUTER_ID=lj532-6iaaa-aaaah-qcc7a-cai
 
 printf "💎 DIP-721 Deploy\n\n"
-printf " 💡 Network: %s,\n 💡 Name: %s,\n 💡 Token: %s,\n 💡 OwnerPrincipalID: \n      %s\n\n" "$NETWORK" "$TOKENNAME" "$TOKENSTRING" "$PRINCIPALID"
+printf "💡 Deploy to network (%s), the token (%s) with name (%s) of owner principal (%s)" "$NETWORK" "$TOKENSTRING" "$TOKENNAME" "$OWNER_PRINCIPAL_ID"
 
-# This only applies to local replica
-if [ -d ./.dfx/local/canisters/nft ]; then
-	printf "🚩 The process seems to have run before, it is recommended to reset dfx and the local replica.\n\n"  
-
-	if [[ ! SKIP_PROMPTS -eq 1 ]]; then
-		read -r -p "🤖 Would you like me to reset the environment? [y/n]? " CONT
-		if [ "$CONT" = "y" ]; then
-			$YARNORNPM reset
-			printf "🤖 Please start your local replica with `dfx start --clean`"
-            exit 0
-		fi
-	fi
+# Overrides default Canister Cap Id
+if [[ $NETWORK == "local" ]]; then
+    source "./.scripts/required/cap-verification.sh"
 fi
 
-CAP_ROUTER_ID_PATH="./.temp/ic-history-router-id"
-
-if [[ $NETWORK == "local" ]];
-then
-    printf "🙏 Verifying the Cap Service status, please wait...\n\n"
-
-    if [ ! -e "$CAP_ROUTER_ID_PATH" ]; then
-        # The extra space is intentional, used for alignment
-        printf "⚠️  Warning: The Cap Service is required.\n\n"
-
-        # The extra space is intentional, used for alignment
-        read -r -p "🤖 Would you like me to start the Cap Service for you [y/n]? " CONT1
-
-        if [ "$CONT1" = "y" ]; then
-            $YARNORNPM cap:start
-        else
-            read -r -p "🤖 Enter the local Cap container ID: " CONT2
-            echo $CONT2 > $CAP_ROUTER_ID_PATH
-            exit 1
-        fi
-    fi
-
-    CANISTER_CAP_ID=$(cat "$CAP_ROUTER_ID_PATH")
-    IS_CAP_SERVICE_RUNNING=$(dfx canister id "$CANISTER_CAP_ID")
-
-    if [ -z "$IS_CAP_SERVICE_RUNNING" ]; then
-        printf "🤖 Oops! The Cap Service Canister (%s) is not running...\n\n" "$CANISTER_CAP_ID"
-
-        exit 1
-    fi
-
-    printf "🌈 Cap Service running as canister id (%s)\n\n" "$CANISTER_CAP_ID"
-
-    CANISTER_CAP_ID=$(cat .temp/ic-history-router-id)
-else
-    CANISTER_CAP_ID=lj532-6iaaa-aaaah-qcc7a-cai
-fi
-
-CANISTER_NFT_ID=$(dfx canister --network $NETWORK id $NFT)
-if [[ ! $? -eq 0 ]]; then
-    printf "🤖 Deploying new canister!\n\n"
-    dfx deploy --network $NETWORK $NFT --argument "(principal \"$PRINCIPALID\", \"$TOKENSTRING\", \"$TOKENNAME\", principal \"$CANISTER_CAP_ID\")"
-    dfx canister --network $NETWORK update-settings $NFT --controller $PRINCIPALID --controller $(dfx canister --network $NETWORK id $NFT)
-    exit 0
-fi
-    
-if [[ "$2" == "reinstall" ]]; then
+if [[ "$MODE" == "reinstall" ]]; then
     printf "🤖 Reinstalling canister!\n\n"
-    dfx deploy --no-wallet --network $NETWORK nft --argument "(principal \"$PRINCIPALID\", \"$TOKENSTRING\", \"$TOKENNAME\", principal \"$CANISTER_CAP_ID\")" -m reinstall
+
+    npm run dip721:deploy-nft "$NETWORK" "$OWNER_PRINCIPAL_ID" "$TOKENSTRING" "$TOKENNAME" "$CAP_HISTORY_ROUTER_ID" "reinstall"
+
     exit 0
 fi
 
-printf "🤖 Redeploying canister!\n\n"
-dfx deploy --no-wallet --network $NETWORK $NFT --argument "(principal \"$PRINCIPALID\", \"$TOKENSTRING\", \"$TOKENNAME\", principal \"$CANISTER_CAP_ID\")"
-exit 0
+npm run dip721:deploy-nft "$NETWORK" "$OWNER_PRINCIPAL_ID" "$TOKENSTRING" "$TOKENNAME" "$CAP_HISTORY_ROUTER_ID"
+
+NFT_CANISTER_ID=$(dfx canister --network "$NETWORK" id nft)
+
+printf "💡 The Non fungible token canister id is (%s)" "$NFT_CANISTER_ID"
+
+npm run dip721:set-controllers "$NETWORK" "$OWNER_PRINCIPAL_ID" "$NFT_CANISTER_ID"
