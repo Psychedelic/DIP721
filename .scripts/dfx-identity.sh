d30d039fa2d0301e172ce5c931ed0bc863fca4ad
oz <ozwaldorf@gmail.com>
2022-02-21 16:01:32 -0500

    feat: add ability to use any identity

--- .scripts/dfx-identity.sh
+++ .scripts/dfx-identity.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DFX_IDENTITY=$(dfx identity whoami)
+
 ALICE_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t alice-temp)
 BOB_HOME=$(mktemp -d 2>/dev/null || mktemp -d -t bob-temp)
 DEFAULT_HOME="$HOME"
@@ -10,7 +12,7 @@ DEFAULT_PRINCIPAL_ID=$(HOME=$HOME dfx identity get-principal)
 
 ALICE_PEM="$ALICE_HOME/.config/dfx/identity/default/identity.pem"
 BOB_PEM="$BOB_HOME/.config/dfx/identity/default/identity.pem"
-DEFAULT_PEM="$HOME/.config/dfx/identity/default/identity.pem"
+DEFAULT_PEM="$HOME/.config/dfx/identity/$DFX_IDENTITY/identity.pem"
 
 printf "🙋‍♀️ Identities\n\n"
 
@@ -20,7 +22,8 @@ printf "👩🏽‍🦰 ALICE_HOME (%s)\n" "$ALICE_HOME"
 printf "👨🏽‍🦰 BOB_PRINCIPAL_ID (%s)\n" "$BOB_PRINCIPAL_ID"
 printf "👨🏽‍🦰 BOB_HOME (%s)\n" "$BOB_HOME"
 
-printf "👨🏾‍💻 DEFAULT_PRINCIPAL_ID (%s)\n" "$DEFAULT_PRINCIPAL_ID"
-printf "👨🏾‍💻 DEFAULT_HOME (%s)\n" "$DEFAULT_HOME"
+printf "👨🏾‍💻 DFX_IDENTITY (%s)\n" "$DFX_IDENTITY"
+printf "👨🏾‍💻 PRINCIPAL_ID (%s)\n" "$DEFAULT_PRINCIPAL_ID"
+printf "👨🏾‍💻 HOME (%s)\n" "$DEFAULT_HOME"
 
 printf "\n\n"
